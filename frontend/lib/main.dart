12e3cc92a037f199efa0996cd23b094bafb2019b
EmmaDanielaP <L21170435@culiacan.tecnm.mx>
2025-10-05 22:06:59 -0700

    Muchos cambios jaja

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'OMR Evaluador',
+      title: 'Evalink',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const OmrScreen(),
     );
